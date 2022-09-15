bb101f78ef800539601d461d2dbcaaa98d32634f
Yun Jang <jang7j@yahoo.com>
2022-09-15 08:58:06 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
+const firstName;
+let lastName = "jang";
+var age = 18;
 
 // Exercise 2
 
+firstName = "yun";
+lastName = "jang";
+age = 19;
+
 // Exercise 3
+let language = "JavaScript";
+let createdYear = "1995";
+let isCaseSentitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
